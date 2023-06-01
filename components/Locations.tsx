ed49124d2c852c5236d7c5f40d4ec65cc38096cf
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-06-01 13:20:25 -0600

    google maps, markers, and Info Window working

--- components/Locations.tsx
+++ components/Locations.tsx
@@ -1,9 +1,14 @@
-import { GoogleMap, LoadScript, Marker, InfoWindow  } from "@react-google-maps/api";
-import { Key } from "react";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import { Key, useState } from "react";
 
 const mapStyles = {
   height: "50vh",
-  width: "100%",
+  width: "50%",
 };
 
 const defaultCenter = {
@@ -15,36 +20,73 @@ export interface LocationsProps {
   locations: any;
 }
 
-
 const Locations: React.FC<LocationsProps> = ({ locations }) => {
+  const [selectedLocation, setSelectedLocation] = useState<{
+    name: string;
+    description: string;
+    locations: {
+      lat: number;
+      lng: number;
+      _key: Key | null | undefined;
+    }[];
+  } | null>(null);
+
   return (
-    <div>
+    <div className="mt-10 mb-10 mx-auto w-1/2 h-1/2">
       <LoadScript googleMapsApiKey="AIzaSyD2_18EQyvbZ8JP-8WdEq3gu3EFz93VqL0">
         <GoogleMap
           mapContainerStyle={mapStyles}
-          zoom={10}
+          zoom={4}
           center={defaultCenter}
         >
           {locations &&
-            locations.map((locationDoc: { locations: { lat: number; lng: number; _key: Key | null | undefined; }[]; }) => {
-              return locationDoc.locations.map(
-                (location: {
+            locations.map(
+              (locationDoc: {
+                locations: {
                   lat: number;
                   lng: number;
                   _key: Key | null | undefined;
-                }) => {
-                  return (
-                    <Marker
-                      key={location._key}
-                      position={{
-                        lat: location.lat,
-                        lng: location.lng,
-                      }}
-                    />
-                  );
-                }
-              );
-            })}
+                }[];
+              }) => {
+                return locationDoc.locations.map(
+                  (location: {
+                    lat: number;
+                    lng: number;
+                    _key: Key | null | undefined;
+                  }) => {
+                    return (
+                      <Marker
+                        key={location._key}
+                        position={{
+                          lat: location.lat,
+                          lng: location.lng,
+                        }}
+                        onClick={() => {
+                          setSelectedLocation(locationDoc as any);
+                        }}
+                      />
+                    );
+                  }
+                );
+              }
+            )}
+
+          {selectedLocation && (
+            <InfoWindow
+              position={{
+                lat: selectedLocation.locations[0].lat,
+                lng: selectedLocation.locations[0].lng,
+              }}
+              onCloseClick={() => {
+                setSelectedLocation(null);
+              }}
+            >
+              <div>
+                <h2>{selectedLocation.name}</h2>
+                <p>{selectedLocation.description}</p>
+              </div>
+            </InfoWindow>
+          )}
         </GoogleMap>
       </LoadScript>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,29 @@
-import { Inter } from 'next/font/google'
-import { client } from '@/sanity/lib/client'
-import Locations from '../components/Locations'
+import { Inter } from "next/font/google";
+import { client } from "@/sanity/lib/client";
+import Locations from "../components/Locations";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export async function getStaticProps() {
-  const locations = await client.fetch('*[_type == "locations"]')
-  return { props: { locations } }
+  const locations = await client.fetch('*[_type == "locations"]');
+  return { props: { locations } };
 }
-
-
 
 interface HomeProps {
-  locations: any
+  locations: any;
 }
-
 
 export default function Home({ locations }: HomeProps) {
+  console.log(locations);
+
   return (
     <main>
-      <h1 className='font-extrabold text-5xl text-center text-blue-500'>
+      <h1 className="font-extrabold text-5xl text-center text-blue-500">
         Hello World
       </h1>
-      
+
       <Locations locations={locations} />
     </main>
-  )
+  );
 }
+
